6903f6908199df0d04368342e632ceb45cdf7050
Pedro Henrique Melo Matos <melomatosp@gmail.com>
2025-06-12 14:11:15 -0300

    estruturando o projeto em flask

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+from flask import Flask, render_template
 import requests
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
 
 url_pura = "http://api.weatherapi.com/v1/current.json"
 chave_api = os.getenv("key")
 
-url = f"{url_pura}?key={chave_api}&q=Guarulhos"
+url = f"{url_pura}?key={chave_api}&q=guarulhos"
 request = requests.get(url)
-clima = request.json()
-print(clima['current']['temp_c'])
+clima_geral = request.json()
+clima_city = clima_geral['current']['temp_c']
 
+if __name__ == '__main__':
+    app.run(debug=True)
